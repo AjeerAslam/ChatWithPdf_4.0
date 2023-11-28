c9a2b984f145703371c6556b37301bbe4b2f1310
ajeeraslam@gmail.com <ajeeraslam@gmail.com>
2023-11-28 13:22:25 +0530

    commit

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from dotenv import load_dotenv
-import pickle
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -8,21 +6,28 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.callbacks import get_openai_callback
 import os
 import time
 # get a token: https://platform.openai.com/account/api-keys
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from bson.objectid import ObjectId
 
 
+#Db loading
+uri = st.secrets['uri']
 
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+db=client['chatbot']
+collection=db['allchats']
 
-
-
-
- 
-load_dotenv()
-
- 
 def main():
     st.header("Chat with PDF 💬")
  
@@ -45,32 +50,15 @@ def main():
             )
         chunks = text_splitter.split_text(text=text)
  
-        # # embeddings
-        store_name = pdf.name[:-4]
-        # st.write(chunks)
- 
-        # if os.path.exists(f"{store_name}.pkl"):
-        #     with open(f"{store_name}.pkl", "rb") as f:
-        #         VectorStore = pickle.load(f)
-        #     # st.write('Embeddings Loaded from the Disk')s
-        # else:
         embeddings = OpenAIEmbeddings()
         VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        # with open(f"{store_name}.pkl", "wb") as f:
-        #     pickle.dump(VectorStore, f)
- 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
- 
- 
-        # Initialize chats
+        
         if "allChats" not in st.session_state:
-            st.session_state.allChats = [[]]
-            st.session_state.currentChat = []
-            st.session_state.chatIndex = 0
+            doc=collection.find_one({"_id": ObjectId("65648061cfa7bb8d4c3d146b")})
+            st.session_state.allChats = doc["allchats"]
+            st.session_state.currentChat = st.session_state.allChats[len(st.session_state.allChats)-1]
+            st.session_state.chatIndex = len(st.session_state.allChats)-1
             
-
-
         with st.sidebar:
             #Create a button to increment and display the number
             if st.button('New chat+'):
@@ -84,15 +72,6 @@ def main():
                     st.session_state.chatIndex=chat
             add_vertical_space(5)
             st.write('Made with ❤️ by Ajeer')
-
-        # st.title("Simple chat")
-        # if st.button('New chat+'):
-        #         st.session_state.chats.append([])
-        # for chat in range(len(st.session_state.chats)):
-        #     #button_key = f'button_{i}'
-        #     if st.button(f'chat-{chat}'):
-        #         st.write(chat)
-
 
         # Display chat messages from history on app rerun
         for message in st.session_state.currentChat:
@@ -116,8 +95,7 @@ def main():
                 docs = VectorStore.similarity_search(query=prompt, k=3)
                 llm = OpenAI()
                 chain = load_qa_chain(llm=llm, chain_type="stuff")
-                with get_openai_callback() as cb:
-                    assistant_response = chain.run(input_documents=docs, question=prompt)
+                assistant_response = chain.run(input_documents=docs, question=prompt)
 
                 # Simulate stream of response with milliseconds delay
                 for chunk in assistant_response.split():
@@ -128,7 +106,13 @@ def main():
                 message_placeholder.markdown(full_response)
             # Add assistant response to chat history
             st.session_state.currentChat.append({"role": "assistant", "content": full_response})
+        
+        query = {"_id": ObjectId("65648061cfa7bb8d4c3d146b")} # define the query
+        update = {"$set": {"allchats": st.session_state.allChats}} # define the update object
+        collection.update_one(query, update)
+
  
 if __name__ == '__main__':
     os.environ["OPENAI_API_KEY"] =st.secrets['OPENAI_API_KEY']
+    
     main()
